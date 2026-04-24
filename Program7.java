91332741c9ac368b07fdfb81d9888ef8002773f6
vishalbambhaniya994-boop <vishalbambhaniya994@gmail.com>
2026-04-24 19:53:57 +0530

    Update Program7.java

--- Program7.java
+++ Program7.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 
 public class Employee {
 
-    // Private instance variables
+    
     private String employeeName;
     private double employeeSalary;
 
-    // Method to read employee data
+    
     public void readEmployeeData() {
         Scanner sc = new Scanner(System.in);
 
@@ -17,19 +17,19 @@ public class Employee {
         employeeSalary = sc.nextDouble();
     }
 
-    // Method to display employee data
+    
     public void displayEmployeeData() {
         System.out.println("\nEmployee Details:");
         System.out.println("Name: " + employeeName);
         System.out.println("Salary: " + employeeSalary);
     }
 
-    // Main method
+    
     public static void main(String[] args) {
-        // Object creation
+        
         Employee emp = new Employee();
 
-        // Method calls
+        
         emp.readEmployeeData();
         emp.displayEmployeeData();
     }
