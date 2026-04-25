57680879651405eb7862dae95427da6fa273b97f
vishalbambhaniya994-boop <vishalbambhaniya994@gmail.com>
2026-04-25 10:05:23 +0530

    Update Program10.java

--- Program10.java
+++ Program10.java
@@ -1,24 +1,24 @@
 public class BankAccount {
 
-    // Instance variables
+    
     private String accountHolderName;
     private double balance;
 
-    // Static variable (common for all accounts)
-    private static double interestRate = 5.0; // default 5%
+  
+    private static double interestRate = 5.0; 
 
-    // Constructor
+    
     public BankAccount(String name, double balance) {
         this.accountHolderName = name;
         this.balance = balance;
     }
 
-    // Method to calculate interest
+    
     public double calculateInterest() {
         return (balance * interestRate) / 100;
     }
 
-    // Method to display account details and interest
+    
     public void display() {
         System.out.println("Account Holder: " + accountHolderName);
         System.out.println("Balance: " + balance);
@@ -27,28 +27,28 @@ public class BankAccount {
         System.out.println();
     }
 
-    // Static method to update interest rate
+    
     public static void updateInterestRate(double newRate) {
         interestRate = newRate;
         System.out.println("Interest rate updated to " + interestRate + "%\n");
     }
 
-    // Main method
+    
     public static void main(String[] args) {
 
-        // Create accounts
+        
         BankAccount acc1 = new BankAccount("Dharmik", 10000);
         BankAccount acc2 = new BankAccount("Rahul", 20000);
 
-        // Display initial details
+       
         System.out.println("Before updating interest rate:");
         acc1.display();
         acc2.display();
 
-        // Update interest rate (static method)
+        
         BankAccount.updateInterestRate(7.5);
 
-        // Display after updating interest rate
+        
         System.out.println("After updating interest rate:");
         acc1.display();
         acc2.display();
