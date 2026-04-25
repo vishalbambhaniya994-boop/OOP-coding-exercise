f2a3c2c87cfe5cb5f8cebc14a55e866b72974a05
vishalbambhaniya994-boop <vishalbambhaniya994@gmail.com>
2026-04-25 10:24:03 +0530

    Update Program22.java

--- Program22.java
+++ Program22.java
@@ -1,20 +1,20 @@
-// Custom Exception
+
 class BookNotAvailableException extends Exception {
     public BookNotAvailableException(String message) {
         super(message);
     }
 }
 
-// Library class
+
 class Library {
     private int availableBooks;
 
-    // Constructor
+    
     public Library(int availableBooks) {
         this.availableBooks = availableBooks;
     }
 
-    // Method to issue books
+    
     public void issueBook(int count) throws BookNotAvailableException {
         if (count <= availableBooks) {
             availableBooks -= count;
@@ -26,18 +26,18 @@ class Library {
     }
 }
 
-// Main class
+
 public class Main {
     public static void main(String[] args) {
 
-        // Initialize library with 3 books
+        
         Library lib = new Library(3);
 
         try {
-            // Valid case
+            
             lib.issueBook(2);
 
-            // Invalid case (should throw exception)
+            
             lib.issueBook(2);
 
         } catch (BookNotAvailableException e) {
