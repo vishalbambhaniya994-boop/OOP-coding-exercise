68182293f3ea22e079f8961023abfe60c0b3de9f
vishalbambhaniya994-boop <vishalbambhaniya994@gmail.com>
2026-04-25 10:18:54 +0530

    Update Program18.java

--- Program18.java
+++ Program18.java
@@ -1,12 +1,12 @@
-// Abstract class
+
 abstract class Vehicle {
 
-    // Abstract methods
+    
     abstract String fuelType();
     abstract int noOfWheels();
 }
 
-// Subclass Car
+
 class Car extends Vehicle {
 
     @Override
@@ -20,7 +20,7 @@ class Car extends Vehicle {
     }
 }
 
-// Subclass Bike
+
 class Bike extends Vehicle {
 
     @Override
@@ -34,21 +34,21 @@ class Bike extends Vehicle {
     }
 }
 
-// Main class
+
 public class Main {
     public static void main(String[] args) {
 
         Vehicle car = new Car();
         Vehicle bike = new Bike();
 
-        // Car details
+        
         System.out.println("Car Details:");
         System.out.println("Fuel Type: " + car.fuelType());
         System.out.println("Number of Wheels: " + car.noOfWheels());
 
         System.out.println();
 
-        // Bike details
+        
         System.out.println("Bike Details:");
         System.out.println("Fuel Type: " + bike.fuelType());
         System.out.println("Number of Wheels: " + bike.noOfWheels());
