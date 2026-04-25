a55a5b2f28c34301807e1bb142ae380b7be3d7de
vishalbambhaniya994-boop <vishalbambhaniya994@gmail.com>
2026-04-25 10:07:01 +0530

    Update Program11.java

--- Program11.java
+++ Program11.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 
 public class College {
 
-    // Outer class data member
+    
     private String collegeName;
 
-    // Constructor for College
+   
     public College(String collegeName) {
         this.collegeName = collegeName;
     }
 
-    // Non-static inner class
+    
     class Admission {
         private String studentName;
         private String course;
 
-        // Method to accept student details
+        
         public void acceptData() {
             Scanner sc = new Scanner(System.in);
 
@@ -26,7 +26,7 @@ public class College {
             course = sc.nextLine();
         }
 
-        // Method to display details
+       
         public void displayData() {
             System.out.println("\n--- Admission Details ---");
             System.out.println("College Name: " + collegeName); // accessing outer class member
@@ -35,16 +35,16 @@ public class College {
         }
     }
 
-    // Main method
+    
     public static void main(String[] args) {
 
-        // Create outer class object
+        
         College college = new College("SSASIT College");
 
-        // Create inner class object
+        
         College.Admission admission = college.new Admission();
 
-        // Input and display
+        
         admission.acceptData();
         admission.displayData();
     }
