06c61493c33b10c1d65d76c08797f32d56502313
vishalbambhaniya994-boop <vishalbambhaniya994@gmail.com>
2026-04-25 10:17:51 +0530

    Update Program17.java

--- Program17.java
+++ Program17.java
@@ -14,7 +14,7 @@ class Result implements Exam, Classify {
     
     @Override
     public boolean isPassed(int mark) {
-        return mark >= 40; // passing criteria
+        return mark >= 40; 
     }
 
     
